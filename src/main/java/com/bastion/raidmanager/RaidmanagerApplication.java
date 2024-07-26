d4d6514a4d25bf77e6fe6167aa83496ee64e5711
Javier Rodriguez Regueiro <javierrodriguezregueiro@gmail.com>
2024-07-26 13:30:09 +0200

    Initial commit with spring boot inilizr files

---
+++ src/main/java/com/bastion/raidmanager/RaidmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.bastion.raidmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaidmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaidmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bastion/raidmanager/RaidmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bastion.raidmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaidmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
